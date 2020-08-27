81838d1e295dc3b4962d74780c9481782c86d27b
crisleo94 <cristhian.reinoso@live.com>
2020-08-27 14:34:37 -0500

    create a public folder with the HTML, CSS and JS static files for the view of the app, create an server.js entry file, add an utils folder with util functions to format messages and users

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, getRoomUsers, userLeave } = require('./utils/users')
+
+const PORT = 3000 || process.env.PORT
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+const botName = 'CinemaChat Bot'
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket) => {
+
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room)
+
+    socket.join(user.room)
+  
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to CinemaChat!!'))
+  
+    // User connected
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat.`))
+
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    })
+  })
+
+  // Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id)
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg))
+  })
+  
+  // User disconnected
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id)
+    
+    if(user) {
+      io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat.`))
+      
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      })
+    }
+
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`)
+})
